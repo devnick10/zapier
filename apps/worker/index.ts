828f08c376d37c3a926482a0bab193fd272d1fb0
devnick10 <nickbhoyar683@gmail.com>
2026-06-06 00:37:30 +0530

    add worker

---
+++ apps/worker/index.ts
@@ -0,0 +1,39 @@
+import { client } from "@repo/db";
+import { Kafka } from "kafkajs";
+import { Topic } from "@repo/events";
+
+const kafka = new Kafka({
+    clientId: "outbox-proccesor",
+    brokers: ["localhost:9092"]
+})
+
+
+async function main() {
+    const consumer = kafka.consumer({ groupId: "main-worker" });
+    await consumer.connect()
+    await consumer.subscribe({
+        topic: Topic.TOPIC_NAME,
+        fromBeginning: true,
+    })
+
+    while (true) {
+        await consumer.run({
+            autoCommit: false,
+            eachMessage: async ({ partition, message }) => {
+                console.log({
+                    partition,
+                    offset: message.offset,
+                    value: message.value?.toString()
+                })
+
+                await consumer.commitOffsets([{
+                    topic: Topic.TOPIC_NAME,
+                    partition,
+                    offset: (parseInt(message.offset) + 1).toString()
+                }])
+            }
+        })
+    }
+}
+
+main().catch(console.error)
